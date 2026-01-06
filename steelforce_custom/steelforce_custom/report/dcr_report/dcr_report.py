2cfc759bd775cc556f1172dfa9954fceb0d53bbf
sivajyothis7 <sivajyothis9446@gmail.com>
2026-01-06 23:25:48 +0300

    dcr_fixed

--- steelforce_custom/steelforce_custom/report/dcr_report/dcr_report.py
+++ steelforce_custom/steelforce_custom/report/dcr_report/dcr_report.py
@@ -5,7 +5,6 @@ import frappe
 
 
 def color_parent_name(name):
-    """Apply color based on sales type"""
     if name.startswith("Online Sales"):
         return f"<span style='color:#2ca02c; font-weight:600'>{name}</span>"
     if name.startswith("Home Sales"):
@@ -16,8 +15,7 @@ def color_parent_name(name):
 
 
 def execute(filters=None):
-    if not filters:
-        filters = {}
+    filters = filters or {}
 
     from_date = filters.get("from_date")
     to_date = filters.get("to_date")
@@ -27,72 +25,47 @@ def execute(filters=None):
     # COLUMNS
     # -------------------------------------------------
     columns = [
-        {
-            "fieldname": "name",
-            "label": "Sales Type / Mode of Payment / Invoice",
-            "fieldtype": "Data",
-            "width": 360
-        },
-        {
-            "fieldname": "amount",
-            "label": "Amount",
-            "fieldtype": "Currency",
-            "width": 180
-        },
-        {
-            "fieldname": "invoice",
-            "label": "Invoice",
-            "fieldtype": "Link",
-            "options": "Sales Invoice",
-            "width": 260
-        }
+        {"fieldname": "name", "label": "Sales Type / Mode of Payment / Invoice", "fieldtype": "Data", "width": 360},
+        {"fieldname": "amount", "label": "Amount", "fieldtype": "Currency", "width": 180},
+        {"fieldname": "invoice", "label": "Invoice", "fieldtype": "Link", "options": "Sales Invoice", "width": 260},
     ]
 
     data = []
-    grand_total = 0  # TOTAL with VAT
+    grand_total = 0
 
     # -------------------------------------------------
-    # 🔹 PARENTS (Sales Type + Mode of Payment)
+    # 🔹 PARENTS (Sales Type + Mode)
     # -------------------------------------------------
     parents = frappe.db.sql("""
         SELECT
-            CASE
-                WHEN si.is_return = 1 THEN
-                    CONCAT(
-                        CASE
-                            WHEN si.customer IN ('HUNGER STATION', 'KETA', 'JAHEZ', 'TO YOU')
-                                THEN 'Online Sales'
-                            WHEN si.customer = 'Walk-in Customer'
-                                THEN 'Counter Sales'
-                            ELSE
-                                'Home Sales'
-                        END,
-                        ' - ',
-                        IFNULL(sip.mode_of_payment, 'Credit Sale'),
-                        ' (Return)'
-                    )
-                ELSE
-                    CONCAT(
-                        CASE
-                            WHEN si.customer IN ('HUNGER STATION', 'KETA', 'JAHEZ', 'TO YOU')
-                                THEN 'Online Sales'
-                            WHEN si.customer = 'Walk-in Customer'
-                                THEN 'Counter Sales'
-                            ELSE
-                                'Home Sales'
-                        END,
-                        ' - ',
-                        IFNULL(sip.mode_of_payment, 'Credit Sale')
-                    )
-            END AS parent_name,
+            CONCAT(
+                CASE
+                    WHEN si.customer IN ('HUNGER STATION','KETA','JAHEZ','TO YOU')
+                        THEN 'Online Sales'
+                    WHEN si.customer = 'Walk-in Customer'
+                        THEN 'Counter Sales'
+                    ELSE 'Home Sales'
+                END,
+                ' - ',
+                IFNULL(sip.mode_of_payment, 'Credit Sale'),
+                IF(si.is_return = 1, ' (Return)', '')
+            ) AS parent_name,
 
             si.is_return,
 
             SUM(
                 CASE
                     WHEN sip.mode_of_payment IS NULL
-                        THEN IFNULL(si.grand_total, 0)
-                    ELSE IFNULL(sip.amount, 0) - IFNULL(si.change_amount, 0)
+                        THEN si.grand_total
+                    WHEN sip.mode_of_payment = 'Cash'
+                        THEN si.grand_total
+                             - IFNULL((
+                                 SELECT SUM(x.amount)
+                                 FROM `tabSales Invoice Payment` x
+                                 WHERE x.parent = si.name
+                                   AND x.mode_of_payment != 'Cash'
+                             ),0)
+                    ELSE sip.amount
                 END
             ) AS amount
 
@@ -108,12 +81,8 @@ def execute(filters=None):
             AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s
 
         GROUP BY parent_name, si.is_return
-        ORDER BY parent_name, si.is_return
-    """, {
-        "from_date": from_date,
-        "to_date": to_date,
-        "pos_profile": pos_profile
-    }, as_dict=True)
+        ORDER BY parent_name
+    """, filters, as_dict=True)
 
     # -------------------------------------------------
     # 🔹 BUILD TREE
@@ -123,7 +92,7 @@ def execute(filters=None):
             "name": color_parent_name(p.parent_name),
             "parent": None,
             "amount": p.amount,
-            "indent": 0
+            "indent": 0,
         })
 
         grand_total += p.amount or 0
@@ -131,85 +100,82 @@ def execute(filters=None):
         sales_type = p.parent_name.split(" - ")[0]
         mode_only = p.parent_name.split(" - ")[-1].replace(" (Return)", "")
 
+        # -------------------------------------------------
+        # 🔹 INVOICES (MODE-WISE FIX)
+        # -------------------------------------------------
         invoices = frappe.db.sql("""
-            SELECT si.name, si.grand_total
+            SELECT
+                si.name,
+                CASE
+                    WHEN %(mode)s = 'Credit Sale'
+                        THEN si.grand_total
+
+                    WHEN %(mode)s = 'Cash'
+                        THEN si.grand_total
+                             - IFNULL((
+                                 SELECT SUM(x.amount)
+                                 FROM `tabSales Invoice Payment` x
+                                 WHERE x.parent = si.name
+                                   AND x.mode_of_payment != 'Cash'
+                             ),0)
+
+                    ELSE sip.amount
+                END AS amount
+
             FROM `tabSales Invoice` si
             LEFT JOIN `tabSales Invoice Payment` sip
                 ON sip.parent = si.name
-                AND sip.parenttype = 'Sales Invoice'
-                AND sip.parentfield = 'payments'
+                AND sip.mode_of_payment = %(mode)s
+
             WHERE
                 si.docstatus = 1
                 AND si.pos_profile = %(pos_profile)s
                 AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s
                 AND si.is_return = %(is_return)s
+
                 AND (
-                    (%(mode)s LIKE '%%Credit Sale%%' AND sip.name IS NULL)
-                    OR IFNULL(sip.mode_of_payment, 'Credit Sale') = %(mode_only)s
+                    (%(mode)s = 'Credit Sale' AND sip.name IS NULL)
+                    OR sip.mode_of_payment = %(mode)s
                 )
+
                 AND (
-                    (
-                        %(sales_type)s = 'Online Sales'
-                        AND si.customer IN ('HUNGER STATION', 'KETA', 'JAHEZ', 'TO YOU')
-                    )
-                    OR
-                    (
-                        %(sales_type)s = 'Counter Sales'
-                        AND si.customer = 'Walk-in Customer'
-                    )
-                    OR
-                    (
-                        %(sales_type)s = 'Home Sales'
+                    (%(sales_type)s = 'Online Sales'
+                        AND si.customer IN ('HUNGER STATION','KETA','JAHEZ','TO YOU'))
+                    OR (%(sales_type)s = 'Counter Sales'
+                        AND si.customer = 'Walk-in Customer')
+                    OR (%(sales_type)s = 'Home Sales'
                         AND si.customer NOT IN (
-                            'HUNGER STATION', 'KETA', 'JAHEZ', 'TO YOU', 'Walk-in Customer'
-                        )
-                    )
+                            'HUNGER STATION','KETA','JAHEZ','TO YOU','Walk-in Customer'))
                 )
+
             ORDER BY si.name
         """, {
-            "from_date": from_date,
-            "to_date": to_date,
-            "pos_profile": pos_profile,
+            **filters,
             "is_return": p.is_return,
-            "mode": p.parent_name,
-            "mode_only": mode_only,
-            "sales_type": sales_type
+            "mode": mode_only,
+            "sales_type": sales_type,
         }, as_dict=True)
 
         for inv in invoices:
-            data.append({
-                "name": inv.name,
-                "invoice": inv.name,
-                "parent": p.parent_name,
-                "amount": inv.grand_total,
-                "indent": 1
-            })
+            if inv.amount:
+                data.append({
+                    "name": inv.name,
+                    "invoice": inv.name,
+                    "parent": p.parent_name,
+                    "amount": inv.amount,
+                    "indent": 1,
+                })
 
     # -------------------------------------------------
-    # 🔹 FINAL SUMMARY (CORRECT FORMULA)
+    # 🔹 FINAL SUMMARY
     # -------------------------------------------------
     vat_amount = round(grand_total * 0.15 / 1.15, 2)
     total_wo_vat = round(grand_total - vat_amount, 2)
 
     data.extend([
-        {
-            "name": "<b>Total W/O VAT</b>",
-            "parent": None,
-            "amount": total_wo_vat,
-            "indent": 0
-        },
-        {
-            "name": "<b>Total VAT (15%)</b>",
-            "parent": None,
-            "amount": vat_amount,
-            "indent": 0
-        },
-        {
-            "name": "<b style='font-size:14px'>TOTAL</b>",
-            "parent": None,
-            "amount": grand_total,
-            "indent": 0
-        }
+        {"name": "<b>Total W/O VAT</b>", "amount": total_wo_vat, "indent": 0},
+        {"name": "<b>Total VAT (15%)</b>", "amount": vat_amount, "indent": 0},
+        {"name": "<b style='font-size:14px'>TOTAL</b>", "amount": grand_total, "indent": 0},
     ])
 
     return columns, data
